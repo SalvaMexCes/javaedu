ca880a272819b087ccba8d54b447e861af3816bc
Cesar Machado <cesarmachado2121@gmail.com>
2024-03-13 22:09:08 -0400

    update

---
+++ src/main/java/RiceCost.java
@@ -0,0 +1,11 @@
+public class RiceCost {
+    public static void main(String[] args) {
+
+        int qty = 20;
+        int price = 5;
+        int miles = 10;
+        int fee = 5;
+        System.out.println("Total cost of user's items is " + (qty * price + fee));
+
+    }
+}
